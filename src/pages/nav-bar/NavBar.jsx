ec4f72c41a7e4fde5f93277a1d66f63fe9de6307
Alan Navarro <josephalannavarro@gmail.com>
2023-11-30 19:19:51 -0600

    style: navbar brand img solved

--- src/pages/nav-bar/NavBar.jsx
+++ src/pages/nav-bar/NavBar.jsx
@@ -1,18 +1,14 @@
 import MenuOption from "../../components/navbar/MenuOption";
-// import LogoImg from "../../assets/img/logo/firmaFerro";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
 	return (
 		<>
 			<nav className="navbar navbar-expand d-none d-md-block py-1" id="navbar">
 				<div className="container-fluid">
-					<a className="navbar-brand ms-3" href="./index.html">
-						<img
-							id="logo-navbar"
-							// src={LogoImg}
-							alt="Logo: firma artista Alfonso Ferro"
-						/>
-					</a>
+					<Link className="navbar-brand ms-3" href="./index.html">
+						<p>FERRO</p>
+					</Link>
 					<div className id="navbarNav">
 						<ul className="navbar-nav fs-6">
 							<MenuOption pageToConnect={"/"}>INICIO</MenuOption>
