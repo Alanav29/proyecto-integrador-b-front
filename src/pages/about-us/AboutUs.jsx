8a964ac1536dec31578321d5b722baf63693417d
Alanav29 <josephalannavarro@gmail.com>
2023-12-03 23:52:05 -0600

    style: homeLogoMobile added

--- src/pages/about-us/AboutUs.jsx
+++ src/pages/about-us/AboutUs.jsx
@@ -3,16 +3,14 @@ import "../../styles/about-us/aboutUs.css";
 import membersArray from "../../utils/about-us/members-info";
 import CardTeamMember from "../../components/about-us/CardTeamMember";
 import cuadroNosotros from "../../assets/img/general/cuadro1Gaby.jpg";
+import teamLogoImg from "../../assets/img/general/htamalerosLogoSinFondo.png";
 
 const AboutUs = () => {
   return (
-    <main>
-      <section
-        className="container py-0 py-md-5 d-flex align-items-center"
-        id="brand-description-container"
-      >
-        <article className="row row-cols-md-2 row-cols-1">
-          <div className="col d-flex flex-column justify-content-center">
+    <main id="about-us-container">
+      <section className="container my-5">
+        <article className="row d-flex align-items-center row-cols-md-2 row-cols-1">
+          <div className="col">
             <h2>Alfonso Ferro</h2>
             <p className="justify">
               Alfonso Ferro es un pintor consagrado de origen colombiano
@@ -25,48 +23,15 @@ const AboutUs = () => {
               lienzo.
             </p>
           </div>
-          <div className="col">
-            <img
-              className="img-fluid rounded-4"
-              src={cuadroNosotros}
-              alt="Cuadro del artista Alonso Ferro con formato horizontal donde destacan los colores amarillos, grises y verdes"
-            />
+          <div className="col d-flex align-items-center">
+            <img className="img-fluid" src={cuadroNosotros} alt="" />
           </div>
         </article>
       </section>
-      <section
-        className="container py-0 py-md-5 d-flex align-items-center"
-        id="team-description-container"
-      >
-        <article className="row row-cols-md-2 row-cols-1">
-          <div className="col d-flex flex-column justify-content-center">
-            <h2>Alfonso Ferro</h2>
-            <p className="justify">
-              Alfonso Ferro es un pintor consagrado de origen colombiano
-              residente en México. Su estilo se caracteriza por la abstracción y
-              fuerza de sus composiciones. Las pinturas se centran en el color,
-              el movimiento y la técnica que utiliza. Con frecuencia, podrás
-              descubrir paisajes escondidos o formas del mundo real dentro de
-              las obras. Trabaja con colores vibrantes y experimenta con
-              diferentes formatos, destacando la técnica del acrílico sobre
-              lienzo.
-            </p>
-          </div>
+
+      <section className="container my-5">
+        <article className="row d-flex align-items-center flex-row-reverse row-cols-md-2 row-cols-1">
           <div className="col">
-            <img
-              className="img-fluid rounded-4"
-              src={cuadroNosotros}
-              alt="Cuadro del artista Alonso Ferro con formato horizontal donde destacan los colores amarillos, grises y verdes"
-            />
-          </div>
-        </article>
-      </section>
-      {/* <section
-        className="container d-flex align-items-center py-5"
-        id="team-description-container"
-      >
-        <article className="row row-cols-md-2 row-cols-1">
-          <div className="col d-flex flex-column justify-content-center order-md-last">
             <h2>Nuestro Equipo</h2>
             <p className="justify">
               Somos un equipo de desarrollo <em>Full-Stack</em> apasionado y
@@ -85,15 +50,12 @@ const AboutUs = () => {
               excepcional que destaque la riqueza y diversidad de la pintura.
             </p>
           </div>
-          <div className="col p-5">
-            <img
-              className="img-fluid rounded-4"
-              src={htamalerosLogoImg}
-              alt="Lootipo del equipo de desarrolo para este proyecto llamado htamaleros"
-            />
+          <div className="col d-flex align-items-center">
+            <img className="img-fluid" src={teamLogoImg} alt="" />
           </div>
         </article>
-      </section> */}
+      </section>
+
       <section>
         <h2 className="text-center">Conoce a nuestro equipo</h2>
         <div
