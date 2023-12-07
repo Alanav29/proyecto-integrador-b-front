54cf75666aa392a68f999c474dbd4739ec61b191
Alanav29 <josephalannavarro@gmail.com>
2023-12-06 21:31:13 -0600

    src: file names updated

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,24 +1,29 @@
 import addProduct from "../../utils/gallery/addProduct";
 import productArray from "../../utils/gallery/prueba";
 import { useState } from "react";
-import Product from "../../utils/gallery/product-class";
+import Product from "../../utils/gallery/productClass";
 
 const Gallery = () => {
-    const [product, setProduct] = useState(new Product({
-        "title": 'Destellos del desierto',
-        "width": 150,
-        "height": 170,
-        "color": "yellow",
-        "price": "$25,000",
-        "technique": "acrylic on canvas"
-    }));
+  const [product, setProduct] = useState(
+    new Product({
+      title: "Destellos del desierto",
+      width: 150,
+      height: 170,
+      color: "yellow",
+      price: "$25,000",
+      technique: "acrylic on canvas",
+    })
+  );
   return (
-    <button onClick={() => { addProduct(product,productArray)
-    console.log(productArray);
-    }}>
-        Gallery
+    <button
+      onClick={() => {
+        addProduct(product, productArray);
+        console.log(productArray);
+      }}
+    >
+      Gallery
     </button>
-  )
-}
+  );
+};
 
-export default Gallery
+export default Gallery;
--- src/utils/gallery/prueba.js
+++ src/utils/gallery/prueba.js
@@ -1,24 +1,23 @@
-import Product from "./product-class";
+import Product from "./productClass";
 
 const product1 = new Product({
-    "title": 'Destellos  del desierto',
-    "width": 150,
-    "height": 170,
-    "color": "yellow",
-    "price": "$25,000",
-    "technique": "acrylic on canvas"
+  title: "Destellos  del desierto",
+  width: 150,
+  height: 170,
+  color: "yellow",
+  price: "$25,000",
+  technique: "acrylic on canvas",
 });
 
 const product2 = new Product({
-    "title": 'Destellos  del desierto',
-    "width": 150,
-    "height": 170,
-    "color": "yellow",
-    "price": "$25,000",
-    "technique": "acrylic on canvas"
+  title: "Destellos  del desierto",
+  width: 150,
+  height: 170,
+  color: "yellow",
+  price: "$25,000",
+  technique: "acrylic on canvas",
 });
 
-const productArray = [product1, product2]
+const productArray = [product1, product2];
 
 export default productArray;
-
