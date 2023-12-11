977168b7aeb164a5ce10ec5d64fe746a161ec0be
Alanav29 <josephalannavarro@gmail.com>
2023-12-10 19:11:29 -0600

    feutures: productClass Restored and Gallery component repaired

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,33 +1,30 @@
-import addProduct from "../../utils/gallery/addProduct";
-import productArray from "../../utils/gallery/prueba";
 import { useState } from "react";
-import Product from "../../utils/gallery/productClass";
+// import Product from "../../utils/gallery/productClass";
 import cuadroImg from "../../assets/img/general/cuadro1Gaby.jpg";
 import ProductListItem from "../../components/galleryAdmin/ProductListItem";
 
 const Gallery = () => {
-  const [product, setProduct] = useState(
-    new Product({
-      title: "Destellos del desierto",
-      width: 150,
-      height: 170,
-      color: "yellow",
-      price: "$25,000",
-      technique: "acrylic on canvas",
-    })
-  );
+  // const [product, setProduct] = useState(
+  //   new Product({
+  //     title: "Destellos del desierto",
+  //     width: 150,
+  //     height: 170,
+  //     color: "yellow",
+  //     price: "$25,000",
+  //     technique: "acrylic on canvas",
+  //   })
+  // );
   return (
     <>
-      <ProductListItem
+      {/* <ProductListItem
         title={product.title}
         img={cuadroImg}
         price={product.price}
         quantity={1}
-      />
+      /> */}
       <button
         onClick={() => {
-          addProduct(product, productArray);
-          console.log(productArray);
+          console.log("boton");
         }}
       >
         Gallery
