065d33275d15674910086b07749fa9acf0b4d200
Alanav29 <josephalannavarro@gmail.com>
2023-12-15 16:26:25 -0600

    style: first changes in gallery and gallery cards

--- src/components/gallery/GalleryProduct.jsx
+++ src/components/gallery/GalleryProduct.jsx
@@ -1,22 +1,19 @@
 import "../../styles/gallery/galleryProduct.css";
 
 const GalleryProduct = ({ product: { title, img, price } }) => {
-  console.log(title, img, price);
   return (
-    <div>
-      <article className="gallery-product d-flex flex-wrap justify-content-center mb-5">
-        <div className="gallery-product-img-container">
-          <img
-            className="img-fluid"
-            src={img}
-            alt={`Imagen sobre la obra ${title}`}
-          />
-        </div>
-        <div className="gallery-product-body">
-          <h2>{title}</h2>
-          <p>{price}</p>
-        </div>
-      </article>
+    <div className="gallery-product p-2">
+      <div className="gallery-product-img-container">
+        <img
+          className="img-fluid"
+          src={img}
+          alt={`Imagen sobre la obra ${title}`}
+        />
+      </div>
+      <div className="gallery-product-body">
+        <h3>{title}</h3>
+        <p>{price}</p>
+      </div>
     </div>
   );
 };
