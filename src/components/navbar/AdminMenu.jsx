407319ea98f81f256a2b0bc32ced556ef44de41a
Alanav29 <josephalannavarro@gmail.com>
2023-12-29 14:40:05 -0600

    feature: close session buttons added

--- src/components/navbar/AdminMenu.jsx
+++ src/components/navbar/AdminMenu.jsx
@@ -1,6 +1,16 @@
-import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { setUser } from "../../features/userFeature";
 
 const AdminMenu = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const closeSession = () => {
+    dispatch(setUser({ isAdmin: false }));
+    navigate("/");
+    localStorage.removeItem("user");
+  };
   return (
     <ul className="p-0">
       <Link className="dropdown-item" aria-current="page" to="/galleryAdmin">
@@ -9,6 +19,9 @@ const AdminMenu = () => {
       <Link className="dropdown-item" aria-current="page" to="/">
         MI PERFIL
       </Link>
+      <button className="dropdown-item" onClick={closeSession}>
+        CERRAR SESION
+      </button>
     </ul>
   );
 };
