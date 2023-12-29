f19406b32157b1dcabea1cf0e8d8d5615c254645
Alanav29 <josephalannavarro@gmail.com>
2023-12-28 23:13:48 -0600

    style: gallery closed

--- src/components/gallery/GalleryProduct.jsx
+++ src/components/gallery/GalleryProduct.jsx
@@ -1,15 +1,20 @@
 import "../../styles/gallery/galleryProduct.css";
+import { Link } from "react-router-dom";
 
 const GalleryProduct = ({ product: { title, img, price } }) => {
   return (
     <article className="product-container">
       <div className="card">
         <img src={img} className="card-img" alt="..." />
-        <div className="card-img-overlay p-2">
-          <div className="gallery-product-card-body">
-            <h5 className="card-title fs-3">{title}</h5>
-            <button className="btn btn-light gallery-product-btn">
-              Más información
+        <div className="card-img-overlay m-0 p-0 d-flex align-items-end">
+          <div className="gallery-product-card-body pb-4">
+            <h4 className="text-white">{title}</h4>
+
+            <button className="gallery-product-btn">
+              Detalles
+              <div className="gallery-product-arrow-btn">
+                <i className="bi bi-arrow-right-short text-dark fs-3"></i>
+              </div>
             </button>
           </div>
         </div>
