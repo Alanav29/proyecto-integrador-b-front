1c96f73ec37135f3f5aff3fad05f66bbea0be8e8
gabriela.ferro <gabriela.ferro2018@hotmail.com>
2024-01-04 12:56:07 -0600

    Modified Home page with carrosel not yet installed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,40 @@
-const Home = () => {
-	return <h1>Home</h1>;
+
+import "../../styles/gallery/galleryStyle.css";
+import GalleryProduct from "../../components/gallery/GalleryProduct";
+import { selectProducts } from "../../features/productsFeature";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import Carrossel from "../../components/carrossel/Carrossel";
+
+// Asegúrate de usar la ruta corrcta
+
+
+const GalleryViewHome = () => {
+  // Usamos el selector selectProducts para acceder al estado de los productos
+  const products = useSelector(selectProducts);
+
+  return (
+    <div>
+      <ul>
+      <main id="gallery" className="container-fluid">
+      <h1 className="mx-3 fs-2">Movimiento, Expresion y Arte</h1>
+      <section><Carrossel></Carrossel></section>
+      <section className="mx-2 mx-sm-4">
+        <ResponsiveMasonry
+          columnsCountBreakPoints={{ 425: 1, 450: 2, 740: 3, 1024: 4 }}
+        >
+          <Masonry>
+            {products.map((product) => {
+              return <GalleryProduct product={product} key={product._id} />;
+            })}
+          </Masonry>
+        </ResponsiveMasonry>
+      </section>
+    </main>
+      </ul>
+    </div>
+  );
 };
 
-export default Home;
+export default GalleryViewHome;
