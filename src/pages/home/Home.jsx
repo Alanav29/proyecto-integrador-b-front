e531fcd37605143750ba0132cfa53e79a017e3ee
Sebastian Montes de Oca <ing.montesdeoca94@gmail.com>
2024-01-04 15:49:28 -0600

    add: branch Home, components/home/Phrase.jsx, pages/Home.jsx changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,40 @@
+import Phrase from "../../components/home/Phrase";
+
+
 const Home = () => {
-	return <h1>Home</h1>;
+
+	return (
+	<main id="home" className="container-fluid">
+		<h1 className="mx-3 fs-2">Home</h1>
+		<section className="mx-2 mx-sm-4">
+			<div class="swiper-slide-contents animated zoomIn" style>
+				<div class="elementor-slide-heading">Alfonso Ferro</div>
+			</div>
+		</section>
+		<section class="elementor-section elementor-inner-section elementor-element elementor-element-6754406c animated-slow elementor-section-boxed elementor-section-height-default elementor-section-height-default animated fadeInUp" data-id="6754406c" data-element_type="section" data-settings="{&quot;animation&quot;:&quot;fadeInUp&quot;}">
+			<div class="elementor-container elementor-column-gap-no">
+				<div class="elementor-row">
+					<div class="elementor-column elementor-col-100 elementor-inner-column elementor-element elementor-element-6feeaf7b" data-id="6feeaf7b" data-element_type="column">
+						<div class="elementor-column-wrap elementor-element-populated">
+							<div class="elementor-widget-wrap">
+								<div class="elementor-element elementor-element-753f936a elementor-widget elementor-widget-text-editor" data-id="753f936a" data-element_type="widget" id="Servicios" data-widget_type="text-editor.default">
+									<div class="elementor-widget-container">
+										<div class="elementor-text-editor elementor-clearfix">
+											<p>
+												Mi estilo de arte abstracto se centra en el color, el movimiento y la técnica que utilizo para transmitirlo. Con frecuencia, podrás descubrir formas del mundo real dentro de mis obras”.
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</main>
+	
+	);
 };
 
 export default Home;
