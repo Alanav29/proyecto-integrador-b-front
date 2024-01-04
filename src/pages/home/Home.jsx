657f380610fe0ab040e9cc0824d54124a0ebad93
Sebastian Montes de Oca <ing.montesdeoca94@gmail.com>
2024-01-04 16:49:27 -0600

    add: merging home with carrosselHome

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import Carrossel from "../../components/carrossel/Carrossel";
+import Phrase from "../../components/home/Phrase";
 
 const Home = () => {
 	return (
 		<div>
 			<h1>Home</h1>
 			<Carrossel></Carrossel>
+			<Phrase></Phrase>
 		</div>
 
 		);
