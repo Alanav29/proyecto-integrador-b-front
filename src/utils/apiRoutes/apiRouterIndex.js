971f0d4eb2a50c12d7b1ca3eb0a9ea4fde8db7dd
Alanav29 <josephalannavarro@gmail.com>
2024-01-04 16:40:53 -0600

    cropper for img added to add product form

--- src/utils/apiRoutes/apiRouterIndex.js
+++ src/utils/apiRoutes/apiRouterIndex.js
@@ -1,4 +1,6 @@
 const apiBaseUrl = "https://generation-server-trial.onrender.com";
+// const apiBaseUrl = "http://localhost:8080";
+
 const apiProductsUrl = `${apiBaseUrl}/api/products/`;
 const apiUsersUrl = `${apiBaseUrl}/api/users`;
 const apiLogInUrl = `${apiBaseUrl}/api/users/login`;
