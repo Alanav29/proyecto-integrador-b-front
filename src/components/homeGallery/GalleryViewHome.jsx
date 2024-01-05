ffa7aad29f32d734dc9a9174c9a975808a39d058
Sebastian Montes de Oca <ing.montesdeoca94@gmail.com>
2024-01-04 23:05:29 -0600

    add: merge with home

--- src/components/homeGallery/GalleryViewHome.jsx
+++ src/components/homeGallery/GalleryViewHome.jsx
@@ -1,5 +1,5 @@
 import "../../styles/gallery/galleryStyle.css";
-import GalleryProduct from "../../components/gallery/GalleryProduct";
+import GalleryProduct from "../gallery/GalleryProduct";
 import { selectProducts } from "../../features/productsFeature";
 import React from 'react';
 import { useSelector } from 'react-redux';
