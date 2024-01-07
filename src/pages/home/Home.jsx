b166650115d677ad5693a4e20b9558397b8f8e0a
Alanav29 <josephalannavarro@gmail.com>
2024-01-07 12:57:14 -0600

    Home first commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,20 +1,15 @@
-import Carrossel from "../../components/carrossel/Carrossel";
+import Carousel from "../../components/carousel/Carousel";
 import Phrase from "../../components/home/Phrase";
 import GalleryViewHome from "../../components/homeGallery/GalleryViewHome";
 
 const Home = () => {
-	return (
-		<div>
-			<Carrossel></Carrossel>
-			<br />
-			<Phrase></Phrase>
-			<br />
-			<GalleryViewHome></GalleryViewHome>
-		</div>
-
-	);
-
-
+  return (
+    <div>
+      <Carousel />
+      <Phrase />
+      <GalleryViewHome />
+    </div>
+  );
 };
 
 export default Home;
