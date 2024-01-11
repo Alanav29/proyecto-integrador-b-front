d2d9e9bccce72cb8572c1663fe1bded67ce71540
Alanav29 <josephalannavarro@gmail.com>
2024-01-11 01:08:44 -0600

    repair margins forms

--- src/pages/editProduct/EditProduct.jsx
+++ src/pages/editProduct/EditProduct.jsx
@@ -4,10 +4,7 @@ import EditProductForm from "../../components/editProduct/EditProductForm";
 const EditProduct = () => {
   return (
     <main className="d-flex pt-4 justify-content-center" id="add-product-page">
-      <section
-        id="add-product-form-container"
-        className="p-4 bg-white mx-3 mt-auto"
-      >
+      <section id="add-product-form-container" className="p-4 bg-white mt-auto">
         <EditProductForm />
       </section>
     </main>
