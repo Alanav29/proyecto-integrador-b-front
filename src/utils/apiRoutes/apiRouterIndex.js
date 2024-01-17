4f88846ab3afcb581ad7691a64097e483e3c9097
Alanav29 <josephalannavarro@gmail.com>
2024-01-17 13:07:04 -0600

    first join with backend completed

--- src/utils/apiRoutes/apiRouterIndex.js
+++ src/utils/apiRoutes/apiRouterIndex.js
@@ -1,5 +1,5 @@
 // const apiBaseUrl = "https://generation-server-trial.onrender.com";
-const apiBaseUrl = "http://localhost:8080";
+const apiBaseUrl = "https://proyecto-integrador-backend.onrender.com";
 
 const apiProductsUrl = `${apiBaseUrl}/api/v1/products`;
 const apiUsersUrl = `${apiBaseUrl}/api/v1/users`;
