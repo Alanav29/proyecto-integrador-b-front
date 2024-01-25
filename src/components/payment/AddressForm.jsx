11bd410aebe2e32f1cc07b03d8cff7dc1f809dfc
Carlos Carranza <Carranza591@gmail.com>
2024-01-24 22:06:06 -0600

    AddSelectOption

--- src/components/payment/AddressForm.jsx
+++ src/components/payment/AddressForm.jsx
@@ -15,7 +15,7 @@ const AddressForm = ({ setAddressInfo }) => {
   return (
     <div className=" m-4" id="address-form">
       <h1>Datos de envío</h1>
-      
+
       <form onSubmit={handleSubmit(onSubmit)}>
         <TextInput
           placeholderText={"Calle, numero, colonia, delegación"}
@@ -33,14 +33,50 @@ const AddressForm = ({ setAddressInfo }) => {
           nameText={"zipcode"}
           register={register}
         />
-        <TextInput
-          placeholderText={"Selecciona un estado"}
-          labelText={"Estado"}
-          id={"state-input"}
-          isRequired={true}
-          nameText={"state"}
-          register={register}
-        />
+
+        <div className="d-flex flex-column-reverse state-select">
+          <select id="estados" name="estados" className="p-2" required={true}>
+            <option value="" disabled selected >
+              Selecciona un estado
+            </option>
+            <option value="Aguascalientes">Aguascalientes</option>
+            <option value="Baja California">Baja California</option>
+            <option value="Baja California Sur">Baja California Sur</option>
+            <option value="Campeche">Campeche</option>
+            <option value="Chiapas">Chiapas</option>
+            <option value="Chihuahua">Chihuahua</option>
+            <option value="cdmx">Ciudad de México</option>
+            <option value="Coahuila">Coahuila</option>
+            <option value="Colima">Colima</option>
+            <option value="Durango">Durango</option>
+            <option value="edomex">Estado de México</option>
+            <option value="Guanajuato">Guanajuato</option>
+            <option value="Guerrero">Guerrero</option>
+            <option value="Hidalgo">Hidalgo</option>
+            <option value="Jalisco">Jalisco</option>
+            <option value="Michoacán">Michoacán</option>
+            <option value="Morelos">Morelos</option>
+            <option value="Nayarit">Nayarit</option>
+            <option value="Nuevo León">Nuevo León</option>
+            <option value="Oaxaca">Oaxaca</option>
+            <option value="Puebla">Puebla</option>
+            <option value="Querétaro">Querétaro</option>
+            <option value="Quintana Roo">Quintana Roo</option>
+            <option value="San Luis Potosí">San Luis Potosí</option>
+            <option value="Sinaloa">Sinaloa</option>
+            <option value="Sonora">Sonora</option>
+            <option value="Tabasco">Tabasco</option>
+            <option value="Tamaulipas">Tamaulipas</option>
+            <option value="Tlaxcala">Tlaxcala</option>
+            <option value="Veracruz">Veracruz</option>
+            <option value="Yucatán">Yucatán</option>
+            <option value="Zacatecas">Zacatecas</option>
+          </select>
+          <label className="mx-2 px-1" for="estados">
+           Estado:
+          </label>
+        </div>
+
         <PhoneInput
           placeholderText={"Agrega tu número telefónico"}
           labelText={"Teléfono"}
